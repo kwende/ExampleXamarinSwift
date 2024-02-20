9c212e18d1f3fe4873efdf5a965f56a81eacd79f
Ben Rush <brush@ocuvera.com>
2024-02-20 14:08:48 -0600

    Push this up so I can pull from Mac.

---
+++ Xamarin/XamarinBinding/ApiDefinition.cs
@@ -0,0 +1,11 @@
+﻿namespace XamarinBindings
+{
+    // @interface AzureCommunicationCallingProxy : NSObject
+    [BaseType(typeof(NSObject))]
+    interface AzureCommunicationCallingProxy
+    {
+        // -(NSString * _Nonnull)helloWorldWithInput:(NSString * _Nonnull)input __attribute__((warn_unused_result("")));
+        [Export("helloWorldWithInput:")]
+        string HelloWorldWithInput(string input);
+    }
+}
